1ac637165e250ed754f0f3ec3d6775084200676f
PikachuEXE <pikachuexe@gmail.com>
2013-04-24 17:33:24 +0800

    - Remove settings for highlighting protected tabs

--- defaults/preferences/defaults.js
+++ defaults/preferences/defaults.js
@@ -24,7 +24,6 @@ pref("extensions.tabkit.tabs.tabMinWidth", 100);
 pref("extensions.tabkit.tabs.closeButtons", 1); // 0: Active tab only, 1: All tabs, 2: None, 3: Button at end of tab strip
 
 //--- Appearance ---
-pref("extensions.tabkit.emphasizeProtectedTabs", true);
 pref("extensions.tabkit.disableSearchBar", false);
 pref("extensions.tabkit.solidBackground", false);
 pref("extensions.tabkit.disableTabGroupColor", false);
